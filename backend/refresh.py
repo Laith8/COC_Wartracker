cc249095247322b1cbf61c719be1b260017c01f2
laith <vorotniakjurij@gmail.com>
2026-08-12 21:17:48 +0200

    added attack support in refresh

--- backend/refresh.py
+++ backend/refresh.py
@@ -18,19 +18,6 @@ def parse_coc_datetime(value: str) -> datetime:
         "%Y%m%dT%H%M%S.%fZ",
     ).replace(tzinfo=timezone.utc)
 
-import functools
-
-def tryer(func):
-    @functools.wraps(func)  # Preserves original function metadata
-    async def wrapper(*args, **kwargs):
-        try:
-            r = await func(*args, **kwargs)
-        except Exception as e:
-            print('err:',e)
-            raise
-        return r
-    return wrapper
-
 async def refresh():
     cclient = ClashClient()
     dbclient = DbClient()
@@ -75,17 +62,66 @@ async def refresh():
             tag=war['opponent']['tag'],
             name=war['opponent']['name'],
         )
-        await dbclient.upsert_war(
+        war_instance = await dbclient.upsert_war(
             our_clan_tag=war['clan']['tag'],
             enemy_clan_tag=war['opponent']['tag'],
             end_time=parse_coc_datetime(war['endTime']),
             start_time=parse_coc_datetime(war['startTime']),
             war_type=WarType.RANDOM,
             size=war['teamSize'],
-            result=WarResult.PENDING if war['state'] in ('preparation', 'in_matchmaking', 'war', 'in_war', 'matched') else WarResult.ENDED_UNKNOWN,
+            result=WarResult.PENDING if war['state'] in ('preparation', 'inMatchmaking', 'war', 'inWar', 'matched') else WarResult.ENDED_UNKNOWN,
         )
+        enemy_playertags = [player['tag'] for player in (await cclient.get_members(war['opponent']['tag']))['items']]
+        enemy_playerdata = [await cclient.get_player(tag) for tag in enemy_playertags]
+        for i in enemy_playerdata:
+            await dbclient.upsert_player(
+                tag=i['tag'],
+                name=i['name'],
+                town_hall=i['townHallLevel'],
+                clan_tag=i['clan']['tag'],
+                king=get_hero_level(i,'Barbarian King'),
+                queen=get_hero_level(i,'Archer Queen'),
+                minion=get_hero_level(i, 'Minion Prince'),
+                warden=get_hero_level(i, 'Grand Warden'),
+                champion=get_hero_level(i, 'Royal Champion'),
+                duke=get_hero_level(i, 'Dragon Duke')
+            )
+        attacks = []
+        for participant in war['opponent']['members']:
+            participant_instance = await dbclient.upsert_war_participant(
+                war_id=war_instance.id,
+                player_tag=participant['tag'],
+                clan_tag=war['opponent']['tag'],
+                map_position=participant['mapPosition'],
+                town_hall=participant['townhallLevel'],
+                attacks_allowed=war['attacksPerMember']
+            )
+        for participant in war['clan']['members']:
+            participant_instance = await dbclient.upsert_war_participant(
+                war_id=war_instance.id,
+                player_tag=participant['tag'],
+                clan_tag=war['clan']['tag'],
+                map_position=participant['mapPosition'],
+                town_hall=participant['townhallLevel'],
+                attacks_allowed=war['attacksPerMember']
+            )
+        for participant in [*war['clan']['members'], *war['opponent']['members']]:
+            attacks = participant.get('attacks')
+            if not attacks:
+                continue
+            for i in attacks:
+                await dbclient.upsert_attack(
+                    war_id=war_instance.id,
+                    attacker_id=i['attackerTag'],
+                    defender_id=i['defenderTag'],
+                    attack_number=i['order'],
+                    stars=i['stars'],
+                    destruction=i['destructionPercentage'],
+                    fresh_hit=True if i['order'] == 1 else False,
+                    cleanup=True if i['order'] > 1 else False,
+                    attack_time=i['duration'],
+                )
     return
-    
 
 async def refresh_loop():
     try:
